822c92c311a344490f13d7a8919bc293545bc808
Kaden2003 <42649930+Kaden2003@users.noreply.github.com>
2018-10-08 10:27:04 -0500

    update2

--- Pig dice/Pig dice/Source.cpp
+++ Pig dice/Pig dice/Source.cpp
@@ -6,17 +6,32 @@ using namespace std;
 
 int main()
 {
-	int lyingPig;
-	int razorBack;
-	int trotter;
-	int snouter;
-	int leaningJowler;
+	int lyingPig = 0;
+	int razorBack = 5;
+	int trotter = 5;
+	int snouter = 10;
+	int leaningJowler = 15;
 	int userChoice;
+	int userChoice2;
 	double scores[5] = { lyingPig, razorBack, trotter, snouter, leaningJowler };
 
-	cout << "Welcome to pig dice! would you like to play?";
+	cout << "Welcome to pig dice! would you like to play? 1 for yes 2 for no";
 	cin >> userChoice;
-	//hello
+	
+	if (userChoice == 1)
+	{
+		cout << "Are you ready? 1 for yes and 2 for no ";
+		cin >> userChoice2;
+	}
+	else if (userChoice == 2)
+	{
+		cout << "Goodbye";
+	}
+	else 
+	{
+		cout << "Invalid";
+	}
+	/*level1();*/
 
 	system("pause");
 	return 0;
