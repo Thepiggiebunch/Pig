14731e47335b49fabff0b09d74780114d31d29b9
Kaden2003 <42649930+Kaden2003@users.noreply.github.com>
2018-10-18 10:00:03 -0500

    update 11

--- Pig dice/Pig dice/Source.cpp
+++ Pig dice/Pig dice/Source.cpp
@@ -28,98 +28,92 @@ using namespace std;
 //}
 
 
-int Score()
+//int Score()
+//{
+//
+//	int ComputerScore = 0;
+//	int FinalScore = 0;
+//	int Turns = 0;
+//	int score[5] = { 0,5,5,10,15 };
+//	int Face;
+//	int userChoice;
+//	int randomNum;
+//	int choice;
+//	int compNum;
+//
+//
+//
+//	
+//
+//
+//	cout << "Are you ready to roll the dice? 1 for yes and 2 for no" << endl;
+//	cin >> userChoice2;
+//
+//	if (userChoice2 == 1)
+//	{
+//		cout << "Press 1 to roll dice: ";
+//		cin >> userChoice2;
+//	}
+//	else
+//	{
+//		cout << "hmm" << endl;
+//	}
+//
+//	srand(time(NULL));
+//	randomNum = rand() % 6 + 1;
+//	compNum = rand() % 6 + 1;
+//	cout << "Your number is:" << randomNum << endl;
+//	cout << "Computers number is: " << compNum << endl;
+//
+//	if (randomNum > compNum)
+//	{
+//		cout << "You go first!" << endl;
+//		points();
+//	}
+//	else if (compNum > randomNum)
+//	{
+//		cout << "Computer goes first!" << endl;
+//
+//	}
+//	else if (compNum == randomNum)
+//	{
+//		cout << "You tied go again!";
+//	}
+//
+//	system("pause");
+//	return 0;
+//
+//}
+	 
+	 
+		int points()
 {
-
-	int PlayerScore = 0;
-	int ComputerScore = 0;
-	int FinalScore = 0;
-	int Turns = 0;
-	int score[5] = { 0,5,5,10,15 };
-	int Face;
-	int userChoice2;
-	int userChoice;
-	int randomNum;
-	int choice;
-	int compNum;
-<<<<<<< HEAD
-	int lyingPig = 0;
-	int razorBack = 5;
-	int trotter = 5;
-	int snouter = 10;
-	int leaningJowle = 15;
-	string piggie;
-	string PigFace[5] = { "lyingPig", "razorBack" ,"trotter" ,"snouter" ,"leaningJowle" };
-	
-	cout << "Are you ready to roll the dice? 1 for yes and 2 for no" << endl;
-	cin >> userChoice2;
-=======
-	string lyingPig;
-	string razorBack;
-	string trotter;
-	string snouter;
-	string leaningJowle;
-	string PigFace[5] = { lyingPig, razorBack ,trotter ,snouter ,leaningJowle };
-	string piggie;
-
+		int lyingPig = 0;
+		int razorBack = 5;
+		int trotter = 5;
+		int snouter = 10;
+		int leaningJowle = 15;
+		int PlayerScore = 0;
+		bool pigroll = true;
+		string PigFace[5] = { "lyingPig", "razorBack" ,"trotter" ,"snouter" ,"leaningJowle" };
+		int intPigFace[5] = { };
+		int randPiggie;
+		
 		srand(time(NULL));
-		piggie = PigFace[rand() % 4];
+		//piggie = PigFace[rand() % 4]; (intPigFace[0] = stoi(piggie)) == 0
+		
+		
+		
+		do {
+		randPiggie = intPigFace[rand() % 4];
+			cout << "Your first pig position is: " << PigFace[randPiggie] << endl;
 
-
-
-		cout << "Are you ready to roll the dice? 1 for yes and 2 for no" << endl;
-		cin >> userChoice2;
->>>>>>> 5aea4ac1cfb2cba9c53dcf1fda6e5557a681b163
-	
-	if (userChoice2 == 1)
-	{
-		cout << "Press 1 to roll dice: ";
-		cin >> userChoice2;
-	}
-	else 
-	{
-		cout << "go away for now this isnt finished" << endl;
-	}
-	
-	 srand(time(NULL));
-	randomNum = rand() % 6 + 1;
-	compNum = rand() % 6 + 1;
-	cout << "Your number is:" << randomNum << endl;
-	cout << "Computers number is: " << compNum << endl;
-
-	 if (randomNum > compNum)
-	{
-		cout << "You go first!" << endl;
-		int score();
-	}
-	else if (compNum > randomNum)
-	{
-		cout << "Computer goes first!" << endl;
-		int score();
-	}
-	else if (compNum == randomNum)
-	{
-		cout << "You tied go again!";
-	}
-	
-	 do {
-		 if (size(PigFace[0]) == )
+		 if (randPiggie == 0)
 		 {
 			 PlayerScore = PlayerScore + 0;
 			 cout << "You get 0 points\n";
 			 cout << "Your Score is " << PlayerScore << endl;
 
-			 sf::Event event;
-			 while (window.pollEvent(event))
-			 {
-				 if (event.type == sf::Event::Closed)
-					 window.close();
-				 else if (event.type == sf::Event::KeyPressed)
-				 {
-					 if (event.key.code == sf::Keyboard::Key::Escape)
-						 window.close();
-					 // ... etc ...
-				 
 			
 			 sf::RenderWindow window(sf::VideoMode(300, 200, 32), "SFML");
 
@@ -144,7 +138,7 @@ int Score()
 			 }
 			 return EXIT_SUCCESS;
 		 }
-		 else if (Face = size(PigFace[1]))
+		 else if (randPiggie == 1)
 		 {
 			 PlayerScore = PlayerScore + 5;
 			 cout << "You get 5 points\n";
@@ -172,7 +166,7 @@ int Score()
 			 }
 			 return EXIT_SUCCESS;
 		 }
-		 else if (Face = size(PigFace[2]))
+		 else if (randPiggie == 2)
 		 {
 			 PlayerScore = PlayerScore + 5;
 			 cout << "You get 5 points\n";
@@ -200,7 +194,7 @@ int Score()
 			 }
 			 return EXIT_SUCCESS;
 		 }
-		 else if (Face = size(PigFace[3]))
+		 else if (randPiggie == 3)
 		 {
 			 PlayerScore = PlayerScore + 10;
 			 cout << "You get 10 points\n";
@@ -229,7 +223,7 @@ int Score()
 			 return EXIT_SUCCESS;
 		 }
 
-		 else if (Face = size(PigFace[4]))
+		 else if (randPiggie == 4)
 		 {
 			 PlayerScore = PlayerScore + 15;
 			 cout << "You get 15 points\n";
@@ -274,19 +268,47 @@ int Score()
 int main()
 {
 	int userChoice3;
-	
+	int userChoice2;
+	int randomNum;
+	int choice;
+	int compNum;
+
 	cout << "Welcome to pig dice! would you like to play? 1 for Singles 2 for Doubles(Not working) : ";
 	cin >> userChoice3;
-	
-	 if (userChoice3 == 1)
-	{
-		 Score();
-	}
-	 else if (userChoice3 == 2)
+
+		cout << "Are you ready to roll the dice? 1 for yes and 2 for no" << endl;
+	 cin >> userChoice2;
+
+	 if (userChoice2 == 1)
 	 {
-		 cout << "Not finished yet! go away" << endl;
+		 cout << "Press 1 to roll dice: ";
+		 cin >> userChoice2;
+	 }
+	 else
+	 {
+		 cout << "hmm" << endl;
 	 }
 
+	 srand(time(NULL));
+	 randomNum = rand() % 6 + 1;
+	 compNum = rand() % 6 + 1;
+	 cout << "Your number is:" << randomNum << endl;
+	 cout << "Computers number is: " << compNum << endl;
+
+	 if (randomNum > compNum)
+	 {
+		 cout << "You go first!" << endl;
+		 points();
+	 }
+	 else if (compNum > randomNum)
+	 {
+		 cout << "Computer goes first!" << endl;
+
+	 }
+	 else if (compNum == randomNum)
+	 {
+		 cout << "You tied go again!";
+	 }
 
 
 	system("pause");
